3df79312e3e527cf98ee03236824db27b88c0cb4
LackoMolnar <molnar.laszlo@students.jedlik.eu>
2024-04-07 13:41:24 +0200

    quiz oldal befejezve

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -21,10 +24,36 @@ continueBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
-
+    
     showQuestions(0);
     questionCounter(1); 
     headerScore();
+}
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+
+goHomeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
 }
 
 let questionCount = 0;
@@ -41,14 +70,14 @@ nextBtn.onclick = () => {
         questionNumb++;
         questionCounter(questionNumb);
 
-        nextBtn.classList.remove('active')
+        nextBtn.classList.remove('active');
     }
     else {
-        console.log('Question Completed')
+        showResultBox();
     }
 }
 
-const optionList = document.querySelector('.option-list')
+const optionList = document.querySelector('.option-list');
 
 // getting questions and options from array //
 function showQuestions(index) {
@@ -68,7 +97,7 @@ function showQuestions(index) {
     }
 } 
 
-function optionSelected(answer){
+function optionSelected(answer) {
     let userAnswer = answer.textContent;
     let correctAnswer = questions[questionCount].answer;
     let allOptions = optionList.children.length;
@@ -83,7 +112,7 @@ function optionSelected(answer){
 
         //if answer incorrect, auto selected correct answer
         for (let i = 0; i < allOptions; i++) {
-            if (optionList.children[i].textContent == correctAnswer){
+            if (optionList.children[i].textContent == correctAnswer) {
                 optionList.children[i].setAttribute('class', 'option correct');
             }
         }
@@ -99,10 +128,34 @@ function optionSelected(answer){
 
 function questionCounter(index) {
     const questionTotal = document.querySelector('.question-total');
-    questionTotal.textContent = `${index} of ${questions.length} Questions`
+    questionTotal.textContent = `${index} a ${questions.length} Kérdésből`;
 }
 
 function headerScore() {
     const headerScoreText = document.querySelector('.header-score');
-    headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`
+    headerScoreText.textContent = `Pont: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `A pontod ${userScore} a ${questions.length}-ből`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+       progressValue.textContent = `${progressStartValue}%`;
+       circularProgress.style.background = `conic-gradient(#008080 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+        if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+    }, speed);
 }
